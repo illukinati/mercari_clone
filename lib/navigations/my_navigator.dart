61046a510d89b4efa4f3a2089be7949eef5698d1
illukinati <ekoprasetyo@gmail.com>
2018-05-30 15:05:43 +0900

    second commit

--- lib/navigations/my_navigator.dart
+++ lib/navigations/my_navigator.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ecommerce_flutter/screens/home_screen.dart';
 import 'package:ecommerce_flutter/screens/item_screen.dart';
-import 'package:ecommerce_flutter/screens/anjay.dart';
 
 class MyNavigator {
   static final routes = <String, WidgetBuilder>{
     '/home': (BuildContext context) => HomePage(),
     '/item': (BuildContext context) => ItemsPage(),
-    '/anjay': (BuildContext context) => Anjay(),
   };
 
   static void goToWelcome(BuildContext context) {
@@ -20,6 +18,6 @@ class MyNavigator {
   }
 
   static void goToItem(BuildContext context) {
-    Navigator.of(context).pushNamed('/anjay');
+    Navigator.of(context).pushNamed('/item');
   }
 }
